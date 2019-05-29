b9b1841497927f4a2670067a66194a0ef28c0837
Damon Spayde <spayde@hendrix.edu>
2019-05-29 17:17:35 -0500

    Update package to use setuptools to configure command
    + Add setup.py to configure group_selector.py:main() as a command line
    executable

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+        name='group_selector',
+        version='0.1',
+        py_modules=['group_selector'],
+        install_requires=[
+            'Click'
+            ],
+        entry_points='''
+            [console_scripts]
+            group_selector=group_selector:main
+        ''',
+)
