759bff87517d38328e8a2b5fc952ad1e02b189c8
Damon Spayde <spayde@hendrix.edu>
2019-06-05 11:47:33 -0500

    Refactor Group tests
    + Declared a setUp function to hold Student declarations

--- test_suite.py
+++ test_suite.py
@@ -24,69 +24,75 @@ class TestClassStudent(unittest.TestCase):
 
 class TestClassGroup(unittest.TestCase):
 
+    def setUp(self):
+        self.student = []
+        self.student.append(
+                group_selector.Student(
+                    id_number=1, last_name="Smith", first_name="John",
+                    gender="m"
+                )
+            )
+        self.student.append(
+                group_selector.Student(
+                    id_number=2, last_name="Doe", first_name="Jim", gender="m"
+                )
+            )
+        self.student.append(
+                group_selector.Student(
+                    id_number=3, last_name="Mills", first_name="Jane",
+                    gender="f"
+                )
+            )
+        self.student.append(
+                group_selector.Student(
+                    id_number=3, last_name="Cook", first_name="Jill",
+                    gender="f"
+                )
+            )
+
+        self.bad_group = [0, 1, 2]
+        self.good_mixed_group = [0, 2, 3]
+        self.good_equal_group = [0, 1, 2, 3]
+        self.good_female_group = [2, 3]
+        self.good_male_group = [0, 1]
+
     def test_bad_group_composition(self):
-        student1 = group_selector.Student(id_number=1, last_name="Smith",
-                                          first_name="John", gender="m")
-        student2 = group_selector.Student(id_number=2, last_name="Doe",
-                                          first_name="Jim", gender="m")
-        student3 = group_selector.Student(id_number=3, last_name="Mills",
-                                          first_name="Jane", gender="f")
         group = group_selector.Group()
-        group.add_student(student1)
-        group.add_student(student2)
-        group.add_student(student3)
+
+        for i in self.bad_group:
+            group.add_student(self.student[i])
 
         self.assertFalse(group.check_composition())
 
     def test_good_mixed_group_composition(self):
-        student1 = group_selector.Student(id_number=1, last_name="Smith",
-                                          first_name="John", gender="m")
-        student2 = group_selector.Student(id_number=2, last_name="Doe",
-                                          first_name="Jill", gender="f")
-        student3 = group_selector.Student(id_number=3, last_name="Mills",
-                                          first_name="Jane", gender="f")
         group = group_selector.Group()
-        group.add_student(student1)
-        group.add_student(student2)
-        group.add_student(student3)
+
+        for i in self.good_mixed_group:
+            group.add_student(self.student[i])
 
         self.assertTrue(group.check_composition())
 
     def test_good_equal_group_composition(self):
-        student1 = group_selector.Student(id_number=1, last_name="Smith",
-                                          first_name="John", gender="m")
-        student2 = group_selector.Student(id_number=2, last_name="Doe",
-                                          first_name="Jim", gender="m")
-        student3 = group_selector.Student(id_number=3, last_name="Mills",
-                                          first_name="Jane", gender="f")
-        student4 = group_selector.Student(id_number=3, last_name="Cook",
-                                          first_name="Jill", gender="f")
         group = group_selector.Group()
-        group.add_student(student1)
-        group.add_student(student2)
-        group.add_student(student3)
-        group.add_student(student4)
+
+        for i in self.good_equal_group:
+            group.add_student(self.student[i])
 
         self.assertTrue(group.check_composition())
 
     def test_good_female_group_composition(self):
-        student3 = group_selector.Student(id_number=3, last_name="Mills",
-                                          first_name="Jane", gender="f")
-        student4 = group_selector.Student(id_number=3, last_name="Cook",
-                                          first_name="Jill", gender="f")
         group = group_selector.Group()
-        group.add_student(student3)
-        group.add_student(student4)
+
+        for i in self.good_female_group:
+            group.add_student(self.student[i])
 
         self.assertTrue(group.check_composition())
 
     def test_good_male_group_composition(self):
-        student1 = group_selector.Student(id_number=1, last_name="Smith",
-                                          first_name="John", gender="m")
-        student2 = group_selector.Student(id_number=2, last_name="Doe",
-                                          first_name="Jim", gender="m")
         group = group_selector.Group()
-        group.add_student(student1)
-        group.add_student(student2)
 
+        for i in self.good_male_group:
+            group.add_student(self.student[i])
+
+        self.assertTrue(group.check_composition())
         self.assertTrue(group.check_composition())
