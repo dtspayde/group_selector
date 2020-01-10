f07defda2df509af82ed0e7b2528561c8bb18da9
Damon Spayde <spayde@hendrix.edu>
2020-01-10 10:45:29 -0600

    Add group_statistics script to repository
    + Altered setup.py to install group_statistics:cli as a script.
    + Added group_statistics to repository.

--- setup.py
+++ setup.py
@@ -3,12 +3,16 @@ from setuptools import setup
 setup(
         name='group_selector',
         version='0.1',
-        py_modules=['group_selector'],
+        py_modules=['group_selector', 'group_statistics'],
         install_requires=[
             'Click>=7.0'
             ],
-        entry_points='''
-            [console_scripts]
-            group_selector=group_selector:cli
-        ''',
+        entry_points={
+            'console_scripts': [
+                'group_selector=group_selector:cli',
+                'group_statistics=group_statistics:cli',
+            ]
+        }
+        # entry_points=''' [console_scripts]
+        # group_selector=group_selector:cli ''',
 )
