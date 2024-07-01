b950cbbc2c9bc30cbfacf062ccff6eb10a6524ac
Damon Spayde <spayde@hendrix.edu>
2024-07-01 12:03:58 -0500

    Add 'noqa' directives to pass ruff checks.

--- group_statistics.py
+++ group_statistics.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# ruff: noqa: SIM113
 from __future__ import annotations
 
 import json
--- tests/test_suite.py
+++ tests/test_suite.py
@@ -1,3 +1,4 @@
+# ruff: noqa: PT027
 from __future__ import annotations
 
 import unittest
